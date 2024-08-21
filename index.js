5aa9f983083ecdeecfde7c91788904a8aa23323f
朱鸿 <zhxhyy@qq.com>
2024-08-21 09:15:09 +0800

    update

--- index.js
+++ index.js
@@ -13,5 +13,30 @@ export const styleInject = (css, { target, tag }) => {
     return
   }
 
-  document.head.append(style)
+  // 保证样式容易被覆盖
+  let styleIndex = -1
+  const firstStyleEl = document.head.querySelector('style')
+  if(firstStyleEl !== null) {
+    styleIndex = Array.from(document.head.children).findIndex((c) => c === firstStyleEl)
+  }
+
+  let styleSheetIndex = -1
+  const firstStyleSheetEl = document.head.querySelector('link[rel=stylesheet]')
+  if(firstStyleSheetEl !== null) {
+    styleSheetIndex = Array.from(document.head.children).findIndex((c) => c === firstStyleSheetEl)
+  }
+
+  if(styleIndex === -1 && styleSheetIndex === -1) {
+    document.head.append(style)
+    return
+  }
+
+  if(styleIndex !== -1 && styleSheetIndex !== -1) {
+    const referenceNode = styleIndex < styleSheetIndex ? firstStyleEl : firstStyleSheetEl
+    document.head.insertBefore(style, referenceNode)
+    return
+  }
+
+  const referenceNode = styleIndex > styleSheetIndex ? firstStyleEl : firstStyleSheetEl
+  document.head.insertBefore(style, referenceNode)
 }
